9c84cb88702835a810a460f041c19d3a88bfe7a7
hanhan1978 <ryo.tomidokoro@gmail.com>
2017-02-08 09:03:29 +0900

    Fix selenium not found Fix docker-compose   -> remove link section (use docker network instead)   -> use container name to access each other

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Dusk\DuskServiceProvider;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if ($this->app->environment('local', 'testing')) {
+            $this->app->register(DuskServiceProvider::class);
+        }
     }
 }
